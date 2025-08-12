e1b78eb18433e740442075861f2fefd3e9f5087c
Vlad-abc <binzaru.vlad.ctin@gmail.com>
2025-08-12 12:16:53 +0200

    Create UserController.java

---
+++ UserController.java
@@ -0,0 +1,27 @@
+package com.example.marketplace.controller;
+
+import com.example.marketplace.model.User;
+import com.example.marketplace.service.UserService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.findAll();
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userService.save(user);
+    }
+}
